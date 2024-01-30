24cfe79c23b3b4f0e0efa2a49731947efe4d4963
Fernando Florenzano Hernandez <f.florenzano94@gmail.com>
2024-01-30 18:11:48 -0300

    lots of improvements

--- src/components/Episode.jsx
+++ src/components/Episode.jsx
@@ -1,34 +1,60 @@
-import React, { useState } from "react";
+import React from "react";
 import * as Tooltip from "@radix-ui/react-tooltip";
+import * as Popover from "@radix-ui/react-popover";
 
 import styles from "./Episode.module.css";
 
-export default function Episode({ style, episode }) {
-  const [open, setOpen] = useState(false);
-
+const Content = ({ episode }) => {
   return (
-    <Tooltip.Provider delayDuration={0}>
-      <Tooltip.Root open={open === false ? undefined : open}>
-        <Tooltip.Trigger asChild>
+    <div className={styles.content}>
+      <h2>{episode.title}</h2>
+      <p>{episode.seasonName}</p>
+    </div>
+  );
+};
+
+export default function Episode({ style, episode }) {
+  return (
+    <>
+      <Tooltip.Provider delayDuration={0}>
+        <Tooltip.Root>
+          <Tooltip.Trigger asChild>
+            <div
+              className={`${styles.trigger} ${styles.desktopTrigger}`}
+              style={style}
+            />
+          </Tooltip.Trigger>
+          <Tooltip.Portal>
+            <Tooltip.Content
+              className={styles.TooltipContent}
+              sideOffset={5}
+              align="start"
+            >
+              <Content episode={episode} />
+              <Tooltip.Arrow className={styles.TooltipArrow} />
+            </Tooltip.Content>
+          </Tooltip.Portal>
+        </Tooltip.Root>
+      </Tooltip.Provider>
+      <Popover.Root>
+        <Popover.Trigger asChild>
           <div
-            className={styles.root}
+            className={`${styles.trigger} ${styles.mobileTrigger}`}
             style={style}
-            onTouchStart={() => setOpen(true)}
           />
-        </Tooltip.Trigger>
-        <Tooltip.Portal>
-          <Tooltip.Content
+        </Popover.Trigger>
+        <Popover.Portal>
+          <Popover.Content
             className={styles.TooltipContent}
             sideOffset={5}
             align="start"
-            onTouchStart={() => setOpen(false)}
+            side="top"
           >
-            <p>{episode.title}</p>
-            <p>{episode.seasonName}</p>
-            <Tooltip.Arrow className={styles.TooltipArrow} />
-          </Tooltip.Content>
-        </Tooltip.Portal>
-      </Tooltip.Root>
-    </Tooltip.Provider>
+            <Content episode={episode} />
+            <Popover.Arrow className={styles.TooltipArrow} />
+          </Popover.Content>
+        </Popover.Portal>
+      </Popover.Root>
+    </>
   );
 }
