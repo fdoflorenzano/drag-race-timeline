1199234b99c7aaf3b31748efaf9e352a14b45c87
Fernando Florenzano <f@designsystems.international>
2025-08-31 22:29:59 -0400

    update 31/08

--- src/lib/weeks.ts
+++ src/lib/weeks.ts
@@ -1,7 +1,7 @@
 import { getWeekNumber, weeksInYear } from "./dates";
 
 const START_DATE = new Date(2009, 0, 1);
-const END_DATE = new Date(2025, 7, 30);
+const END_DATE = new Date(2025, 8, 28);
 
 export const weeks: {
   [key: string]: { year: number; week: number; weekIndex: number };
